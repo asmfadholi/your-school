2c5193ea32733da4bab33ac93fd0e61856c37a65
asmfadholi <=>
2020-01-18 11:03:44 +0700

    setup initial project

--- src/stores/store.js
+++ src/stores/store.js
@@ -4,13 +4,11 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {
 
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+    }
 })
